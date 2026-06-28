6f127ff414c4ffeb29ed9a9b7fc91d652629a99b
Darshan <darshanpatel200417@gmail.com>
2026-06-28 01:08:43 -0400

    finished all entitys

--- src/main/java/com/trackerFlow/app/task/Task.java
+++ src/main/java/com/trackerFlow/app/task/Task.java
@@ -6,6 +6,9 @@ import com.trackerFlow.app.project.ProjectStatus;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @Table(name="Task")
@@ -42,5 +45,15 @@ public class Task {
     @JoinColumn(name="assigned_project_member",nullable=false)
     private ProjectMember assignedMember;
 
-    
+    @Column(name="start_date",nullable=true)
+    private LocalDate startDate;
+
+    @Column(name="end_date",nullable=true)
+    private LocalDate endDate;
+
+    @Column(name="created_at",nullable=false)
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at",nullable=false)
+    private LocalDateTime updatedAt;
 }
