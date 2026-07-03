ce778c3d4662cd801960161ee4eac196770ca29d
Darshan <darshanpatel200417@gmail.com>
2026-07-02 23:25:07 -0400

    add projectrepo

--- src/main/java/com/trackerFlow/app/organization/OrganizationService.java
+++ src/main/java/com/trackerFlow/app/organization/OrganizationService.java
@@ -1,7 +1,14 @@
 package com.trackerFlow.app.organization;
 
+import com.trackerFlow.app.user.User;
 import com.trackerFlow.app.user.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static com.trackerFlow.app.organization.OrganizationStatus.ACTIVE;
 
 @Service
 public class OrganizationService {
@@ -15,8 +22,37 @@ public class OrganizationService {
         this.userRepository = userRepository;
         this.organizationMemberRepository = organizationMemberRepository;
     }
+    @Transactional
+    OrganizationResponseDto createOrganization(CreateOrganizationRequest request,Long userId) throws Exception {
+        if(organizationRepository.existsByName(request.getName())){
+            throw new Exception("Name already taken");
+        }
+        User currentUser = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("No account found"));
 
 
+        Organization organization = new Organization();
+        organization.setName(request.getName());
+        organization.setDescription(request.getDescription());
+        organization.setStatus(OrganizationStatus.ACTIVE);
+        organization.setCreatedAt(LocalDateTime.now());
+        organization.setUpdatedAt(LocalDateTime.now());
+
+        Organization savedOrganization = organizationRepository.save(organization);
+
+        OrganizationMember organizationMember= new OrganizationMember();
+        organizationMember.setOrganization(savedOrganization);
+        organizationMember.setUser(currentUser);
+        organizationMember.setStatus(OrganizationMemberStatus.ACTIVE);
+        organizationMember.setRole(OrganizationMemberRole.OWNER);
+        organizationMember.setJoinedAt(LocalDateTime.now());
+        organizationMember.setUpdatedAt(LocalDateTime.now());
+
+        OrganizationMember savedOrganizationMember=organizationMemberRepository.save(organizationMember);
+
+        return OrganizationResponseDto;
+
+    }
 
 
 }
