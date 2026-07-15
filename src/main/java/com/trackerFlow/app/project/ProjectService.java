0b57bede2c19fdb2b8ae668411863e295562651d
Darshan <darshanpatel200417@gmail.com>
2026-07-15 11:54:51 -0400

    added createproject func

--- src/main/java/com/trackerFlow/app/project/ProjectService.java
+++ src/main/java/com/trackerFlow/app/project/ProjectService.java
@@ -1,7 +1,9 @@
 package com.trackerFlow.app.project;
 
 import com.trackerFlow.app.dto.request.CreateProjectRequest;
+import com.trackerFlow.app.dto.request.UpdateProjectRequest;
 import com.trackerFlow.app.dto.response.ProjectResponseDto;
+import com.trackerFlow.app.dto.response.UpdatedResponseDto;
 import com.trackerFlow.app.organization.*;
 import com.trackerFlow.app.user.UserRepository;
 import org.springframework.stereotype.Service;
@@ -86,14 +88,21 @@ public class ProjectService {
 
     }
 
-
+    @Transactional(readOnly = true)
     public ProjectResponseDto getProject(Long organizationId,Long projectId,Long currentUserId){
         Organization currentOrganization=organizationRepository.findById(organizationId)
                 .orElseThrow(()->new RuntimeException("Organization not found"));
 
+        if(currentOrganization.getStatus()==OrganizationStatus.ARCHIVED){
+            throw new RuntimeException("Organization is archived");
+        }
+
         Project currentProject=projectRepository.findById(projectId)
                 .orElseThrow(()-> new RuntimeException("Project not found"));
 
+        if(currentProject.getStatus()==ProjectStatus.ARCHIVED){
+            throw new RuntimeException("Project archived");
+        }
         OrganizationMember currentOrganizationMember=organizationMemberRepository.findByOrganizationIdAndUserIdAndStatus(organizationId,currentUserId,OrganizationMemberStatus.ACTIVE)
                 .orElseThrow(()-> new RuntimeException("Not organization member found"));
 
@@ -103,4 +112,81 @@ public class ProjectService {
 
         return toProjectResponseDto(currentProject);
     }
+
+    @Transactional
+    public UpdatedResponseDto updateProject(UpdateProjectRequest request,Long currentUserId, Long projectId, Long organizationId) throws Exception {
+        Organization currentOrganization=organizationRepository.findById(organizationId)
+                .orElseThrow(()-> new RuntimeException("organization not found"));
+
+        if(currentOrganization.getStatus()==OrganizationStatus.ARCHIVED){
+            throw new RuntimeException("Organization is archived");
+        }
+
+        Project currentProject=projectRepository.findById(projectId)
+                .orElseThrow(()->new RuntimeException("project not found"));
+
+        if(currentProject.getStatus()==ProjectStatus.ARCHIVED){
+            throw new RuntimeException("Project archived");
+        }
+
+        OrganizationMember organizationMember=organizationMemberRepository.findByOrganizationIdAndUserIdAndStatus(organizationId,currentUserId,OrganizationMemberStatus.ACTIVE)
+                .orElseThrow(()-> new RuntimeException("organization member not found"));
+
+        if(!Objects.equals(currentOrganization.getOwner().getId(), currentUserId)&&(
+                organizationMember.getRole()!=OrganizationMemberRole.ADMIN)){
+            throw new Exception("You are not allowed to update a project");
+        }
+
+        String name=request.name().trim();
+        if(projectRepository.existsByOrganizationAndNameIgnoreCase(currentOrganization,name)){
+            throw new Exception("Name already exists");
+        }
+        if(name.isBlank()){
+            throw new Exception("Name cannot be empty");
+        }
+
+        currentProject.setName(name);
+        currentProject.setDescription(request.description());
+        currentProject.setUpdatedAt(LocalDateTime.now());
+        projectRepository.save(currentProject);
+
+        return new UpdatedResponseDto(
+                currentProject.getName(), currentProject.getDescription()
+        );
+
+    }
+
+
+    @Transactional
+    public void archiveProject(Long currentUserId,Long organizationId,Long projectId) throws Exception {
+        Organization currentOrganization=organizationRepository.findById(organizationId)
+                .orElseThrow(()-> new RuntimeException("organization not found"));
+
+        if(currentOrganization.getStatus()==OrganizationStatus.ARCHIVED){
+            throw new RuntimeException("Organization is archived");
+        }
+
+        Project currentProject=projectRepository.findById(projectId)
+                .orElseThrow(()->new RuntimeException("project not found"));
+
+        if(currentProject.getStatus()==ProjectStatus.ARCHIVED){
+            throw new RuntimeException("Project archived already");
+        }
+
+        OrganizationMember organizationMember=organizationMemberRepository.findByOrganizationIdAndUserIdAndStatus(organizationId,currentUserId,OrganizationMemberStatus.ACTIVE)
+                .orElseThrow(()-> new RuntimeException("organization member not found"));
+
+        if(!Objects.equals(currentOrganization.getOwner().getId(), currentUserId)&&(
+                organizationMember.getRole()!=OrganizationMemberRole.ADMIN)){
+            throw new Exception("You are not allowed to update a project");
+        }
+
+        currentProject.setStatus(ProjectStatus.ARCHIVED);
+        currentProject.setUpdatedAt(LocalDateTime.now());
+        projectRepository.save(currentProject);
+    }
+
+
+
+
 }
